fa880a38008b3489bd5f50a25017e8f5b24a2a6b
adambauer123 <35768345+adambauer123@users.noreply.github.com>
2018-01-26 11:05:47 -0500

    worthless

--- Video Game/BasicTower.java
+++ Video Game/BasicTower.java
@@ -12,8 +12,11 @@ public class BasicTower extends Actor
      * Act - do whatever the BasicTower wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int count = 0;
     public void act() 
     {
-        // Add your action code here.
-    }    
+        move(1);
+        count++;
+        
+}
 }
