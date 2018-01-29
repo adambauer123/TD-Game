d5881df464a68d3acea4be876b9e8777c0bdd33f
adambauer123 <35768345+adambauer123@users.noreply.github.com>
2018-01-29 11:30:39 -0500

    Major Changes

---
+++ Video Game/Enemy2.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy2 extends enemy
+{
+    /**
+     * Act - do whatever the Enemy2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int health = 1000;
+    public void act() 
+    {
+        MoveAroundMap();
+        health -= 1;
+        if(checkHp(health) == true) {
+            getWorld().removeObject(this);
+        
+    }    
+}
+        
+}
